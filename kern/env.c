91e5c0c587eaba55104c306a14050ef42bda48e7
yilin_huang <942519438@qq.com>
2022-11-07 16:54:27 +0800

    test

--- kern/env.c
+++ kern/env.c
@@ -356,8 +356,9 @@ load_icode(struct Env *e, uint8_t *binary)
 	for (; ph < eph; ++ ph){
 		if( ph->p_type == ELF_PROG_LOAD){
 			region_alloc(e, (void*)ph->p_va, ph->p_memsz);
-			lcr3(PADDR(e->env_pgdir));
+			
 			memset((void*)ph->p_va, 0,ph->p_memsz);
+			lcr3(PADDR(e->env_pgdir));
 			memcpy((void*)ph->p_va, binary + ph->p_offset, ph->p_filesz);
 			lcr3(PADDR(kern_pgdir));
 		}
